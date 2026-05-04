75ca68e6bf90256fffc57a3671ae70b702a0ab65
kingl1231332-code <kingl1231332@gmail.com>
2026-05-04 17:38:04 +0530

    Add files via upload

---
+++ xz.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main ()
+
+{
+	int marks =70;
+	
+	if(marks >= 99){
+	printf("Grade  A");
+    }else if(marks >= 80){
+    	printf("Grade B");
+	}else if(marks >= 50){
+		printf("Grade C");
+    }else if(marks >= 35){
+    	printf("Grade D");
+	}else{
+		printf("We Will Meet Next Time Bro and Sis !!!!!!");
+	}
+    
+}
