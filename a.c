f6b49bb2ff59abdff2e9ec9be7965c5ec7a243ba
kingl1231332-code <kingl1231332@gmail.com>
2026-05-04 14:09:24 +0530

    Add files via upload

---
+++ a.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#define a 700
+int main()
+{
+//	const int a = 700;
+	float k;
+//	a = 750;
+	k = a +45.50;
+	printf("value of k : %.2f",k);
+}
---
+++ s.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main()
+{
+	const int a = 700;
+	float k;
+//	a = 750;
+	k = a +45.50;
+	printf("value of k : %.3f",k);
+}
